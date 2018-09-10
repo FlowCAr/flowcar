ccac654f78831021a0188a68dd6b82e9ad7d4943
FlowCAr <43145657+FlowCAr@users.noreply.github.com>
2018-09-10 18:48:45 +0200

    Add files via upload

---
+++ FlowCAr/R/limListGen.R
@@ -0,0 +1,30 @@
+limListGen <- function(limfile,...)
+{
+  require(LIM)
+  require(enaR)
+  cat("Generating Networks
+")
+
+  AllNetworks <- Xsample(limfile, ...)
+
+  lengthA <- nrow(AllNetworks)
+  fullListMat <-  list(NULL)
+  returnNetworks <-  list(NULL)
+  stepi <- 0
+  pb = txtProgressBar(min = 0, max = lengthA, initial = 0)
+  for (var in 1:as.numeric(lengthA))
+  {
+    stepi = stepi + 1
+    setTxtProgressBar(pb,stepi)
+
+    fullListMat[[(var)]] <- Flowmatrix(limfile,web=AllNetworks[var,])
+  }
+
+  returnNetworks[[1]] <- AllNetworks
+  returnNetworks[[2]] <- fullListMat
+  names(returnNetworks) <-  c("AllNetworks","FlowMatrices")
+  cat("
+Networks created")
+
+  return(returnNetworks)
+}
